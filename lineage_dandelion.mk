1184212d21f47cf77a9ad0ee07d38f92ba3d08f4
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    dandelion: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- lineage_dandelion.mk
+++ lineage_dandelion.mk
@@ -6,6 +6,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit some common LineageOS stuff.
 TARGET_BOOT_ANIMATION_RES := 720
