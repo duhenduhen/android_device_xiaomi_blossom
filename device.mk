7d5127ec752b364af2a36ce7a11cede4619944aa
dianlujitao <dianlujitao@lineageos.org>
2022-09-14 19:11:32 +0100

    dandelion: Build `libsensorndkbridge` from source
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -262,6 +262,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0.vendor \
     android.frameworks.sensorservice@1.0
 
+PRODUCT_PACKAGES += \
+    libsensorndkbridge
+
 # Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
