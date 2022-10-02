6a0c19048551c0ef289de7bd64e49ec3e4b6cdb3
Alexander Koskovich <akoskovich@pm.me>
2022-10-02 21:38:39 +0530

    dandelion: Move BT profiles props to vendor
     * Makes it GSI compatible
    
    Change-Id: I8f7caf241d508af59c16482e6185c42f23f8a460
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,7 +106,6 @@ TARGET_BOARD_PLATFORM := mt6765
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
 # Recovery
