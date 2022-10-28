cb1dbbdde29c8237424ae32e98c0be9d98eab139
bengris32 <bengris32@protonmail.ch>
2022-10-28 20:06:47 +0100

    dandelion: Ensure nonblocking ffs is disabled everywhere
    * Move the persist.adb.nonblocking_ffs property into
     PRODUCT_DEFAULT_PROPERTY_OVERRIDES to ensure its set everywhere.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -68,4 +68,5 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.adb.nonblocking_ffs=0
+    ro.adb.nonblocking_ffs=0 \
+    persist.adb.nonblocking_ffs=0
