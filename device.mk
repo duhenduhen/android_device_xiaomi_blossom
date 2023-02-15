a25e0331e4c6526c72ea374e22352ed435396945
Robert Shih <robertshih@google.com>
2023-02-15 23:54:22 +0530

    dandelion: Enable AIDL DRM Clearkey HAL
    Change-Id: Ib8c5e0f5d50a2764f7f9efc8c92f195428fc25ac
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -68,7 +68,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm-service.clearkey
 
 PRODUCT_PACKAGES += \
     libdrmclearkeyplugin \
