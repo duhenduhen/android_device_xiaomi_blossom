97d3a544075865eabe4bf29298e80768e34286ed
Sushrut1101 <guptasushrut@gmail.com>
2023-04-13 17:29:02 +0530

    dandelion: Define architecture flags
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,3 +5,13 @@
 #
 
 DEVICE_PATH := device/xiaomi/dandelion
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+TARGET_USES_64_BIT_BINDER := true
