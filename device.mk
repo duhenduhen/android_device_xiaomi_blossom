6a7b4bc9496e45e1e2034fe811154e1bfb9d476e
Sushrut1101 <guptasushrut@gmail.com>
2023-04-17 22:31:22 +0530

    dandelion: Build `libavservices_minijail` from source
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -103,6 +103,9 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery
 
 # Media
+PRODUCT_PACKAGES += \
+    libavservices_minijail.vendor \
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_mediatek_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_mediatek_audio.xml \
