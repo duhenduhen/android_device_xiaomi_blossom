f49acbc03e1c7f695afb5677dfe9374d974e72fc
Sushrut1101 <guptasushrut@gmail.com>
2023-04-19 05:55:43 +0530

    dandelion: Build `libtinyxml`
     E vndksupport: Could not load /vendor/lib/hw/audio.primary.mt6765.so from default namespace: dlopen failed: library "libtinyxml.so" not found: needed by /vendor/lib/hw/audio.primary.mt6765.so in namespace (default).
    
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -39,7 +39,8 @@ PRODUCT_PACKAGES += \
     audio_policy.stub \
     libaudiofoundation.vendor \
     libaudiopreprocessing \
-    libtinycompress
+    libtinycompress \
+    libtinyxml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
