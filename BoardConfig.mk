2b1b269b74fd189055c393a70c3426c6e031e86d
Sushrut1101 <guptasushrut@gmail.com>
2023-05-02 17:49:56 +0530

    dandelion: Go 64 bit!
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,14 +7,19 @@
 DEVICE_PATH := device/xiaomi/dandelion
 
 # Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
-TARGET_USES_64_BIT_BINDER := true
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := angelica,angelicain,angelican,cattail,dandelion
