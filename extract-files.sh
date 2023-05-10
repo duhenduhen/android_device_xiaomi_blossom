e972a6a799432b6b7bfaf580d861fb6426599772
Sushrut1101 <guptasushrut@gmail.com>
2023-05-10 16:01:56 +0530

    dandelion: proprietary-files: Drop some useless stuff
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -63,9 +63,6 @@ function blob_fixup {
 	vendor/bin/hw/android.hardware.keymaster@4.0-service.beanpod)
             "${PATCHELF}" --add-needed "libshim_beanpod.so" "${2}"
             ;;
-	vendor/etc/init/mi_*.rc)
-            sed -i '/seclabel/d' "${2}"
-            ;;
 	vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
             "$PATCHELF" --replace-needed "android.hardware.power-V1-ndk_platform.so" "android.hardware.power-V1-ndk.so" "${2}"
             ;;
