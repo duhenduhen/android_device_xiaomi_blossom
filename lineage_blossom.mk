a9cc7160239bf37bd56b390c9d63a766080ac850
Adrian DC <radian.dc@gmail.com>
2023-06-06 13:53:45 +0200

    blossom: Migrate from full configurations to minimal one
    * LatinIME dictionaries extensions from LineageOS are excluded
    * Backgrounds minimal variant saves around 7MB
    * Additional Google Lato and Rubik fonts are excluded
    * Recorder app already requested in the product targets
    
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit some common LineageOS stuff.
 TARGET_BOOT_ANIMATION_RES := 720
-$(call inherit-product, vendor/lineage/config/common_full_go_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_mini_go_phone.mk)
 
 # Inherit from blossom device
 $(call inherit-product, device/xiaomi/blossom/device.mk)
