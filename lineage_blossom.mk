55452e8f8a51a5e3404795b833384f134194b4d0
Sushrut1101 <guptasushrut@gmail.com>
2023-06-29 18:08:55 +0530

    blossom: Inherit Android GO configuration
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -7,11 +7,12 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit some common LineageOS stuff.
 TARGET_BOOT_ANIMATION_RES := 720
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_go_phone.mk)
 
 # Inherit from blossom device
 $(call inherit-product, device/xiaomi/blossom/device.mk)
