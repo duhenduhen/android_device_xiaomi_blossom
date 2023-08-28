bfdd6f3bbd212801953971c5a7fa05cd092b2dbd
Sushrut1101 <guptasushrut@gmail.com>
2023-08-28 13:16:44 +0530

    blossom: Drop unused `libSoftGatekeeper`
    dandelion:/# grep -ir libSoftGatekeeper /vendor
    
    /vendor/lib/hw/gatekeeper.default.so:libSoftGatekeeper.so
    grep: /vendor/lib/hw/libSoftGatekeeper.so: binary file matches
    /vendor/lib64/hw/gatekeeper.default.so:libSoftGatekeeper.so
    grep: /vendor/lib64/hw/libSoftGatekeeper.so: binary file matches
    
    Since this lib is symlinked to gatekeeper.default.so and we have
    ro.hardware.gatekeeper=beanpod, gatekeeper.default.so will not even be
    loaded! Instead, gatekeeper.${ro.hardware.gatekeeper}.so will be loaded.
    
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- Android.mk
+++ Android.mk
@@ -7,20 +7,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),blossom)
+
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
-
-VENDOR_SYMLINK := \
-    $(TARGET_OUT_VENDOR)/lib/hw \
-    $(TARGET_OUT_VENDOR)/lib64/hw
-
-$(VENDOR_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	$(hide) echo "Making vendor symlinks"
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
-	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
-	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
-	$(hide) touch $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINK)
 
 endif
