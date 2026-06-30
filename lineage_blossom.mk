fed1c2cdffea60f0c50353adf29d3f4114e52a8a
Andrey Klimets <duhenduhenduhen@gmail.com>
2026-06-30 17:38:47 +0300

    blossom: drop useless flags
    Signed-off-by: Andrey Klimets <duhenduhenduhen@gmail.com>

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -18,36 +18,6 @@ $(call inherit-product, device/xiaomi/blossom/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# always nuke audiofx
-NUKE_AUDIOFX := true
-
-# always append time of day
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
-
-# AxionAOSP flags start
-# Camera
-AXION_CAMERA_REAR_INFO := 13
-AXION_CAMERA_FRONT_INFO := 5
-PRODUCT_NO_CAMERA := false
-
-# Processor
-AXION_PROCESSOR := Mediatek_Helio_G25_/_G35
-
-# Maintainer
-AXION_MAINTAINER := fukiame
-
-# Perf anim override
-PERF_ANIM_OVERRIDE := true
-
-# LOS apps
-TARGET_INCLUDES_LOS_PREBUILTS := true
-
-# CPU governor support
-PERF_GOV_SUPPORTED := true
-PERF_DEFAULT_GOV := schedutil
-
-# AxionAOSP flags end
-
 PRODUCT_NAME := lineage_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
