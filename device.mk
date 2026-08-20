e8dd51c3a96ac943b8fbc1649a2508675823f06e
Woomymy <woomy@woomy.be>
2026-08-21 00:32:20 +0300

    blossom: Switch to Pixel Thermal AIDL service
    * Config based off "dumpsys thermalservice" using stock thermal service.

--- device.mk
+++ device.mk
@@ -423,10 +423,10 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl
+    android.hardware.thermal-service.mediatek
 
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0.vendor
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/thermal,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # USB
 PRODUCT_PACKAGES += \
